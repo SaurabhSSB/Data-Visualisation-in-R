18766fab7a02613f73456abb0a8472571f68e7f5
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-05 09:35:19 +0530

    Adding File

---
+++ Pie Chart.R
@@ -0,0 +1,26 @@
+# Pie Chart in R language
+# A pie chart is a circular chart divided into sectors, each representing 
+# a category of data. The size of each sector is proportional to the quantity 
+# or frequency of the data it represents.
+
+# Data
+a <- c(14, 41, 36, 44)
+labels <- c("Cherry","Banana","Apple","Mango")
+pie(a,labels)
+pie(a,labels,main="Fruit Chart",col = rainbow(length(a)))
+colours <- c("blue","yellow","red","orange")
+pie(a,labels,main = "Fruit Chart",col=colours)
+
+# Chart Legend
+# legend(x,y=NULL,legend,fill,col,bg) - General Syntax
+
+legend("topright",c("Cherry","Banana","Apple","Mango"),cex=0.9,
+       fill=colours)
+
+# 3-D Pie Chart
+# install.packages("plotrix") - First time only
+library(plotrix)
+b <- c(30,25,45,55)
+labelsa=c("East","West","North","South")
+pie3D(b,labels=labelsa,explode = 0.2 , main = "Directions")
+
