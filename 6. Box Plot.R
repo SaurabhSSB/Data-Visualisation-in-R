6ee050f1195fc764340e2fc332c8e301d5b5c40e
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-11 02:25:54 +0530

    Updating Content

--- 6. Box Plot.R
+++ 6. Box Plot.R
@@ -6,9 +6,10 @@ boxplot(mtcars$hp)
 
 boxplot(mtcars$hp, main= "Data", xlab= "X- Axis", ylab= "Y- Axis", col= "yellow",
         border= "red", horizontal= TRUE, notch= TRUE)
-
 boxplot(mpg ~ cyl, data= mtcars, xlab= "Quantity", ylab= "Miles", col= "yellow",
         border= "red")
+
+
 
 boxplot(mpg ~ cyl, data= mtcars, xlab= "Quantity", ylab= "Miles", main="Box",
         notch= TRUE, varwidth= TRUE, col= c("red", "yellow", "blue"), border= "green",
