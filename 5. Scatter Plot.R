faef679c364720dba7cea3b16da61035daa1a653
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-08 03:42:30 +0530

    Updating Content

--- 5. Scatter Plot.R
+++ 5. Scatter Plot.R
@@ -12,15 +12,13 @@ print(data)
 
 # Plotting the chart for cars with weight height between 2.5 and 5 and mileage
 # between 15 and 30
+
 plot( x= data$wt, y= data$mpg,xlab= "weight", ylab= "Mileage", 
       xlim= c(2.5, 5), ylim= c(15, 30),
       main= "Weight v/s Mileage")
 
 # Scatter Plot
 # install.packages("ggplot2") - First time only
-
-
-
 
 library(ggplot2) 
 names( mtcars)
